3a1fe2bf342899c50775509b11bd3b2abff7d9a3
Patrick Ingham <pingham1991@gmail.com>
2023-10-08 17:40:36 -0700

    Created forms/ for data handling consistency

---
+++ forms/__init__.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""
+Data passing to and from the mongoDB backend will be handled through methods
+outlined in this directory, to preserve consistency.
+
+---
+
+Copyright 2023 Patrick Ingham, Crystal Calvert
+
+This file is part of CubeRubric.
+
+CubeRubric is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+CubeRubric is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along
+with Cuberubric. If not, see <https://www.gnu.org/licenses/>.
+"""
+import os
+import sys
+import yaml
+import logging
+import datetime
+
+import mongoengine as me
+
